52931da8e95c16f2254bdbdba1159e37caaf0850
rangkeswaha <c14180243@john.petra.ac.id>
2020-04-22 09:49:17 +0700

    3 files modified, 4 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { Router } from '@angular/router'
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent  {
   name = 'Angular';
+
+  constructor(private router : Router) {}
+  Homedua(){
+    this.router.navigate(['/home2']);
+  }
 }
